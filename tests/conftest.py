a6fa3c86c74fc08b28d7fd7cce57558c32e59b07
Nadezhda_Popova <nadezhdapopova13@yandex.ru>
2025-07-09 01:43:08 +0700

    created src/models.py, tests/conftest.py, modified main.py

---
+++ tests/conftest.py
@@ -0,0 +1,58 @@
+import pytest
+
+from src.models import Product, Category
+
+
+@pytest.fixture
+def product_1():
+    return Product(
+        name="Xiaomi Redmi Note 11",
+        description="1024GB, Синий",
+        price=31000.0,
+        quantity=14
+    )
+
+
+@pytest.fixture
+def product_2():
+    return Product(
+        name="Samsung Galaxy C23 Ultra",
+        description="256GB, Серый цвет, 200MP камера",
+        price=180000.0,
+        quantity=5
+    )
+
+
+@pytest.fixture
+def category_1():
+    return Category(
+        name="Смартфоны",
+        description="Смартфоны для удобства жизни",
+        products=[
+            Product(
+                "Samsung Galaxy C23 Ultra",
+                "256GB, Серый цвет, 200MP камера",
+                180000.0,
+                5),
+            Product(
+                "Xiaomi Redmi Note 11",
+                "1024GB, Синий",
+                31000.0,
+                14)
+        ]
+    )
+
+
+@pytest.fixture
+def category_2():
+    return Category(
+        name="Телевизоры",
+        description="Ваш друг и помощник",
+        products=[
+            Product(
+                "QLED 4K",
+                "Фоновая подсветка",
+                123000.0,
+                7)
+        ]
+    )
