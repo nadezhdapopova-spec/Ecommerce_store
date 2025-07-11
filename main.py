4c1858094ba790c16c261dd7b4722946320c788e
Nadezhda_Popova <nadezhdapopova13@yandex.ru>
2025-07-12 01:04:13 +0700

    modified models.py, main.py

--- main.py
+++ main.py
@@ -77,3 +77,18 @@ if __name__ == "__main__":
     print(new_product.price)
     new_product.price = 0
     print(new_product.price)
+
+    new_product = Product.new_product(
+        {"name": "Samsung Galaxy S23 Ultra",
+         "description": "256GB, Серый цвет, 200MP камера",
+         "price": 181000.0,
+         "quantity": 5},
+        [
+            Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5),
+            Product("Iphone 15", "512GB, Gray space", 210000.0, 8),
+            Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+        ]
+    )
+    print(new_product.name)
+    print(new_product.quantity)
+    print(new_product.price)
