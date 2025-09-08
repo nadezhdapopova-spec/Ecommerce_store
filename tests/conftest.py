0173f75a37493f5af4db69ce40ab5684a796755c
Nadezhda_Popova <nadezhdapopova13@yandex.ru>
2025-09-08 16:01:48 +0700

    updated .toml

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,7 +13,24 @@ def product_1() -> Product:
         quantity=14
     )
 
+@pytest.fixture
+def product_1() -> Product:
+    return Product(
+        name="Xiaomi Redmi Note 11",
+        description="1024GB, Синий",
+        price=31000.0,
+        quantity=14
+    )
 
+
+@pytest.fixture
+def product_2() -> Product:
+    return Product(
+        name="Samsung Galaxy C23 Ultra",
+        description="256GB, Серый цвет, 200MP камера",
+        price=180000.0,
+        quantity=5
+    )
 @pytest.fixture
 def product_2() -> Product:
     return Product(
